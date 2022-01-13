9374247ce77d0aa1740883a31b8d33ca9ad9fdb4
K2Da <id.k2da@gmail.com>
2022-01-13 20:16:53 +0900

    player

---
+++ src/routes/player/detail/index.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { base } from '$app/paths'
+  import { player_hash } from './index_store'
+
+  onMount(async () => {
+    player_hash.set(new URLSearchParams(window.location.search).get('p'))
+    console.log($player_hash)
+  })
+</script>
+
+<main>
+  <p>{$player_hash}</p>
+  <a href="{base}/player/proto">back</a>
+</main>
