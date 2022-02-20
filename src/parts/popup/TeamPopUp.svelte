978ee5720e4c7e5b6cc4c9b8c38ea343710c62f5
K2Da <id.k2da@gmail.com>
2022-02-20 18:16:14 +0900

    team popup fin

--- src/parts/popup/TeamPopUp.svelte
+++ src/parts/popup/TeamPopUp.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import type { TeamIndex } from '$lib/../../../../center_ping/src/api/TeamIndex'
+  import type { TeamIndex } from '$lib/api/TeamIndex'
   import T from '/src/parts/T.svelte'
-  import S from '/src/parts/S.svelte'
   import Date from '/src/parts/Date.svelte'
+  import TournamentResult from '/src/parts/TournamentResult.svelte'
 
   export let team: TeamIndex
 </script>
@@ -10,4 +10,15 @@
 <h3>{team.name}</h3>
 <p>
   <T t="大会登録名 " />{team.names.join(", ")}
+</p>
+
+<h4>最新大会</h4>
+<p>
+  <Date date={team.latest_tournament_date} spacing={false} /> {team.latest_tournament_name}
+  <TournamentResult rank={team.latest_tournament_rank} />
+</p>
+
+<h4>メンバー</h4>
+<p>
+  {team.members.join(", ")}
 </p>
