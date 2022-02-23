268f508c448f4d74a881d4038d04cb59bce695f9
K2Da <id.k2da@gmail.com>
2022-02-23 16:18:42 +0900

    redirect

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,10 +4,12 @@
 
 <script lang="ts">
   import { base } from '$app/paths'
-  import { loadMaster } from '$lib/store/global'
   import { onMount } from 'svelte'
 
-  onMount(async () => loadMaster())
+  onMount(async () => {
+    // window.location.href = window.location.href.replace('localhost:3000/center_pin_g', 'center-ping.pages.dev')
+    window.location.href = window.location.href.replace('k2da.github.io/center_pin_g', 'center-ping.pages.dev')
+  })
 </script>
 
 <svelte:window bind:innerWidth={$windowWidth} bind:innerHeight={$windowHeight} />
