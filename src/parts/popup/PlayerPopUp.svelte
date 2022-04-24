59f746890e6de79a92fb0399372c751f69069393
K2Da <id.k2da@gmail.com>
2022-04-25 06:05:01 +0900

    21

--- src/parts/popup/PlayerPopUp.svelte
+++ src/parts/popup/PlayerPopUp.svelte
@@ -3,6 +3,9 @@
   import T from '/src/parts/T.svelte'
   import S from '/src/parts/S.svelte'
   import Date from '/src/parts/Date.svelte'
+  import Twitter from '/src/parts/link/Twitter.svelte'
+  import Youtube from '/src/parts/link/Youtube.svelte'
+  import Twitch from '/src/parts/link/Twitch.svelte'
 
   export let player: PlayerIndex
 </script>
@@ -26,4 +29,11 @@
   <p>
     <T t="チーム " />{player.latest.team}
   </p>
+{/if}
+{#if player.data}
+  <td class="tal" colspan="2">
+    <Twitter account={player.data?.twitter} />
+    <Youtube channel={player.data?.youtube && player.data.youtube[0]} name={player.data?.youtube && player.data.youtube[1]} />
+    <Twitch account={player.data?.twitch} />
+  </td>
 {/if}
