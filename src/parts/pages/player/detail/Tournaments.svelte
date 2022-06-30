16ba5886716a941cee225da917c6fca55e38ae63
K2Da <id.k2da@gmail.com>
2022-06-30 18:22:35 +0900

    past rank

--- src/parts/pages/player/detail/Tournaments.svelte
+++ src/parts/pages/player/detail/Tournaments.svelte
@@ -7,8 +7,9 @@
   import PlayersLine from '/src/parts/PlayersLine.svelte'
   import TeamName from '/src/parts/TeamName.svelte'
 
-  function rate(t: any) {
+  function rate(t: null | number) {
     if (t == null) return "-"
+
     return t.toLocaleString()
   }
 </script>
@@ -24,7 +25,7 @@
             <Date date={t.tournament_date} spacing={false} />
           </td>
           <td class="nw"><TournamentResult rank={t.team_result} />
-          <td class="nw"><T>{rate(t.rating)}</T></td>
+          <td class="nw">{#if t.rating == null}-{:else}{rate(t.rating)} <T>({rate(t.rank)}位 / {rate(t.total)}人)</T>{/if}</td>
         </tr>
         <tr>
           <td colspan="3" class="tal">
@@ -41,7 +42,7 @@
         <th>開催日</th>
         <th class="tal">大会名</th>
         <th>結果</th>
-        <th>rating</th>
+        <th colspan="3">rating (順位 / 全体人数)</th>
         <th class="tal">登録チーム名</th>
         <th class="tal">メンバー</th>
       </tr>
@@ -53,6 +54,8 @@
           <td class="tal"><a href="#{t.tournament_key}">{t.tournament_name}</a></td>
           <td><TournamentResult rank={t.team_result} /></td>
           <td>{rate(t.rating)}</td>
+          <td class="tar">{rate(t.rank)}</td>
+          <td class="tar">{rate(t.total)}</td>
           <td class="tal">
             <TeamName name={t.team_name} current_name={t.team_current_name} />
           </td>
